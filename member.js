50352a35a6595f6a441572e8b0b552b4f5fa86c5
JCayllahuaG <u202019558@upc.edu.pe>
2024-06-07 00:35:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMember() {
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["HTML", "CSS", "JS"],
+        "full name": "John Doe"
+    };
+    return member;
+}
